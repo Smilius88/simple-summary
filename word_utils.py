c45b34b2abec3dc9e456714ddc52f7939ee4f282
Sean Young <seanyorickyoung@gmail.com>
2014-10-18 09:44:20 -0400

    Started work on word utils.

--- word_utils.py
+++ word_utils.py
@@ -1,28 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords, wordnet
-from collections import Counter
+def html_plaintext(addr):
+	import requests
+	from bs4 import BeautifulSoup
 
-def getWords(addr):
 	page = requests.get(addr)
-	words = word_tokenize(BeautifulSoup(page.text).get_text())
+	return BeautifulSoup(page.text).get_text()
+
+
+
+def common_words(text):
+	from nltk.tokenize import word_tokenize
+	from nltk.corpus import stopwords, wordnet
+	from collections import Counter
+
+	words = []
+	for word in word_tokenize(text):
+		if word.isalpha():
+			words.append(word.lower())
 	english_stops = set(stopwords.words('english'))
-	words = [word.lower() for word in words if word not in english_stops]
+	words = [word for word in words if word not in english_stops]
 	nouns, verbs, adverbs, adject = [], [], [], []
-	for word in words:
-		if not word.isalpha():
-			continue
-		try:
-			syn = wordnet.synsets(word)[0]
-			c = syn.pos
-			if c == 'n': nouns.append(word)
-			elif c == 'v': verbs.append(word)
-			elif c == 'a': adject.append(word)
-			elif c == 'r': adverbs.append(word)
-		except IndexError:
-			# print word
-			continue
+	try:
+		syn = wordnet.synsets(word)[0]
+		c = syn.pos
+		if c == 'n': nouns.append(word)
+		elif c == 'v': verbs.append(word)
+		elif c == 'a': adject.append(word)
+		elif c == 'r': adverbs.append(word)
+	except IndexError:
+		print word
 
 	wordlist = {}
 	wordlist['nouns'] = Counter(nouns)
